2221fdaff80c98979fa13b707e617b0668b45cdc
JacquesN <j.nguyen14216@gmai.com>
2021-06-28 13:52:53 +0200

    table name

--- migrations/Version20210402121702.php
+++ migrations/Version20210402121702.php
@@ -20,12 +20,12 @@ final class Version20210402121702 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE candidat ADD first_name VARCHAR(255) NOT NULL, ADD last_name VARCHAR(255) NOT NULL, ADD create_at DATETIME NULL, ADD education LONGTEXT NOT NULL, ADD experience LONGTEXT DEFAULT NULL, ADD description_candidat LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE Candidat ADD first_name VARCHAR(255) NOT NULL, ADD last_name VARCHAR(255) NOT NULL, ADD create_at DATETIME NULL, ADD education LONGTEXT NOT NULL, ADD experience LONGTEXT DEFAULT NULL, ADD description_candidat LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE candidat DROP first_name, DROP last_name, DROP create_at, DROP education, DROP experience, DROP description_candidat');
+        $this->addSql('ALTER TABLE Candidat DROP first_name, DROP last_name, DROP create_at, DROP education, DROP experience, DROP description_candidat');
     }
 }
