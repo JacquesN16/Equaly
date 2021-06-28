68bd7dab62ba7481fea15a61f55df00e891e9a0f
JacquesN <j.nguyen14216@gmai.com>
2021-06-28 13:18:24 +0200

    table name

--- src/Entity/Candidat.php
+++ src/Entity/Candidat.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=CandidatRepository::class)
- * @ORM\Table(name="heroku_a60c3cefb9302c8.candidat")
+ * @ORM\Table(name="candidat")
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class Candidat implements UserInterface, \Serializable
